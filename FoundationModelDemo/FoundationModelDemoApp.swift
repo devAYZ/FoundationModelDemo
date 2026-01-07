35e979903dd9a592849e70e94d10cee3dd35a2b1
devayz <fatokimiemman@gmail.com>
2026-01-07 07:38:53 +0100

    Initial Commit

---
+++ FoundationModelDemo/FoundationModelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelDemoApp.swift
+//  FoundationModelDemo
+//
+//  Created by Ayokunle Fatokimi on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoundationModelDemoTests/FoundationModelDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelDemoTests.swift
+//  FoundationModelDemoTests
+//
+//  Created by Ayokunle Fatokimi on 07/01/2026.
+//
+
+import Testing
+@testable import FoundationModelDemo
+
+struct FoundationModelDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
